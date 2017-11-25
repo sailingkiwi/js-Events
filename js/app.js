baa6c1bf1dd1222a933487c4d3b9dff357a90b59
Ritsu <sailingkiwi@gmail.com>
2017-11-24 19:10:09 -1000

    new

--- js/app.js
+++ js/app.js
@@ -5,36 +5,78 @@ The function will add the following content inside the paragraph with the id of 
 */
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
+function moreContent(){
+	var moreBacon = document.getElementById("more");
+	moreBacon.innerHTML=bacon;
+	console.log(bacon);
+};
 
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
-
+function lessContent(){
+	var lessLess = document.getElementById("less");
+	lessLess.innerHTML="";
+	console.log("");
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
+function zoom(){
+	var bigFont = document.getElementById("biggie");
+	bigFont.style.fontSize = "150%";
+	
+}
 
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
+var menuItems = ["Ice cream", "French fries", "Mac shake"];
+function valueMenu (){
+	var menuThree = document.getElementById("menu");
+	menuThree.innerHTML = menuItems;
+	console.log(menuItems);
+
+}
 
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
-
+function redFace(){
+	var newText = document.getElementById("drink");
+	newText.style.color = "red";
+	newText.style.fontSize = "30px";
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
-
+function showPrice(){
+	var addPrice = document.getElementById("price");
+	addPrice.innerHTML = "$5.55";
+	console.log(showPrice);
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+
+var myButton = document.getElementById('displayQuote');
+  myButton.innerHTML = myQuote;
+  function display(){
+  	console.log(myButton);
+  }
+
+  
+myButton.addEventListener('click', display);
+
+
+
+
 
 
 //8. Say It again, Randomly
